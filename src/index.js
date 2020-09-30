00c3c50eb09ff7f202b0752916ab1950a43f9ca7
VadimND <webzorka24@gmail.com>
2020-09-30 22:56:33 +0300

    feat: add 3 functions for arrays

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array) {  
+  let res = Math.min.apply(null, array);
+  if (array == null || array == '') {
+    res = 0;
+  } 
+  return res;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let res = Math.max.apply(null, array);
+  if (array == null || array == '') {
+    res = 0;
+  } 
+  return res;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let sum = 0;
+  let res;
+
+  if (array == undefined || array == null || array == '') {
+    res = 0;
+  } else {
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];    
+    }    
+    res = sum/array.length;
+  }
+  return res;
 }
